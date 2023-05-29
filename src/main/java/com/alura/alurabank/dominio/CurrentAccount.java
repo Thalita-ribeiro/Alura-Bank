850397afa6b33c776f878acae402e02e5a23b987
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2023-05-28 22:35:30 -0300

    Created CurrentAccount class

--- src/main/java/com/alura/alurabank/dominio/CurrentAccount.java
+++ src/main/java/com/alura/alurabank/dominio/CurrentAccount.java
@@ -1,2 +1,18 @@
-package com.alura.alurabank.dominio;public class CurrentAccount {
+package com.alura.alurabank.dominio;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CurrentAccount {
+    @JsonProperty
+    private String bank;
+    @JsonProperty
+    private String agency;
+    @JsonProperty
+    private String number;
+
+    public CurrentAccount(String bank, String agency, String number) {
+        this.bank = bank;
+        this.agency = agency;
+        this.number = number;
+    }
 }
