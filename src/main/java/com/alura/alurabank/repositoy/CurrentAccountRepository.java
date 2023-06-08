6b7e53a80a173fd655639c396fd02045976333f9
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2023-06-08 20:36:14 -0300

    Creating repository

--- src/main/java/com/alura/alurabank/repositoy/CurrentAccountRepository.java
+++ src/main/java/com/alura/alurabank/repositoy/CurrentAccountRepository.java
@@ -1,4 +1,27 @@
-package com.alura.alurabank.respositoy;
+package com.alura.alurabank.repositoy;
 
+import com.alura.alurabank.dominio.CurrentAccount;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
 public class CurrentAccountRepository {
+    private Set<CurrentAccount> currentAccount;
+
+    public CurrentAccountRepository() {
+        currentAccount = new HashSet<>();
+    }
+
+    public void save(CurrentAccount account) {
+        currentAccount.add(account);
+    }
+
+    public Optional<CurrentAccount> search(String bank, String agency, String number) {
+        return currentAccount.stream()
+                .filter(currentAccount -> currentAccount.indertifiedBy(bank, agency, number)).findFirst();
+    }
 }
