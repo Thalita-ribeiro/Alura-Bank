4e98a243a46b8820d63feb3daa6594e1fdd76710
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2023-06-08 20:34:45 -0300

    Rafactoring controller

--- src/main/java/com/alura/alurabank/controller/CurrentAccountController.java
+++ src/main/java/com/alura/alurabank/controller/CurrentAccountController.java
@@ -3,6 +3,8 @@ package com.alura.alurabank.controller;
 import com.alura.alurabank.dominio.AccountHolder;
 import com.alura.alurabank.dominio.AccountTransaction;
 import com.alura.alurabank.dominio.CurrentAccount;
+import com.alura.alurabank.repositoy.CurrentAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,21 +15,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import java.util.Random;
+
+import static java.lang.Integer.MAX_VALUE;
+
 @RestController
 @RequestMapping("/accounts")
 public class CurrentAccountController {
+
+    @Autowired
+    private CurrentAccountRepository currentAccountRepository;
 
     @GetMapping
     public String checkBalance(
             @RequestParam(name = "bank") String bank,
             @RequestParam(name = "agency") String agency,
             @RequestParam(name = "number") String number) {
-        return String.format("Bank: %s, Agency: %s, Account: %s. Balance: R$1380,00", bank, agency, number);
+
+        CurrentAccount currentAccount =
+                currentAccountRepository.search(bank, agency, number)
+                        .orElse(new CurrentAccount());
+
+        return String.format("Bank: %s, Agency: %s, Account: %s. Balance: %s.", bank, agency, number, currentAccount.readBalance());
     }
 
     @PostMapping
     public ResponseEntity<CurrentAccount> createNewAccount(@RequestBody AccountHolder accountHolder) {
-        CurrentAccount account = new CurrentAccount("111", "2222", "700");
+        String bank = "111";
+        String agency = "2222";
+        String number = Integer.toString(new Random().nextInt(MAX_VALUE));
+        CurrentAccount account = new CurrentAccount(bank, agency, number, accountHolder);
+        currentAccountRepository.save(account);
         return ResponseEntity.status(HttpStatus.CREATED).body(account);
     }
 
---
+++ src/main/java/com/alura/alurabank/repositoy/CurrentAccountRepository.java
@@ -0,0 +1,4 @@
+package com.alura.alurabank.respositoy;
+
+public class CurrentAccountRepository {
+}
