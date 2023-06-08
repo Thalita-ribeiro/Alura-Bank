83ab47d5946bfc88c52fd3e6a42164724d175f90
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2023-06-08 20:37:26 -0300

    Creating method to use in controller

--- src/main/java/com/alura/alurabank/dominio/CurrentAccount.java
+++ src/main/java/com/alura/alurabank/dominio/CurrentAccount.java
@@ -1,6 +1,11 @@
 package com.alura.alurabank.dominio;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.ZERO;
 
 public class CurrentAccount {
     @JsonProperty
@@ -9,10 +14,28 @@ public class CurrentAccount {
     private String agency;
     @JsonProperty
     private String number;
+    @JsonProperty
+    private BigDecimal balance;
+    @JsonIgnore
+    private AccountHolder accountHolder;
 
-    public CurrentAccount(String bank, String agency, String number) {
+    public CurrentAccount(String bank, String agency, String number, AccountHolder accountHolder) {
+        this();
         this.bank = bank;
         this.agency = agency;
         this.number = number;
+        this.accountHolder = accountHolder;
+    }
+
+    public CurrentAccount() {
+        this.balance = ZERO;
+    }
+
+    public boolean indertifiedBy(String bank, String agency, String number) {
+        return this.bank.equals(bank) && this.agency.equals(agency) && this.number.equals(number);
+    }
+
+    public BigDecimal readBalance() {
+        return balance;
     }
 }
